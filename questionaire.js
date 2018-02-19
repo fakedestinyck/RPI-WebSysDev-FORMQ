adc7845f6fe1afb86278e9c90a5d0ac9486e8205
obrisa <obrisa@rpi.edu>
2018-02-19 17:37:35 -0500

    Updated Questionnaire
    Added Questions from group meeting and more CSS (bootstrap columns etc to separate columns)

--- questionaire.js
+++ questionaire.js
@@ -1,4 +1,63 @@
       $(document).ready(function(){
+          //FOR ALL OF THE SLIDERS
+          //reference https://www.w3schools.com/howto/howto_js_rangeslider.asp
+          //scheduleslider
+          var slider1 = document.getElementById("schedule");
+          var output1 = document.getElementById("scheduleoutput");
+          output1.innerHTML = slider1.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider1.oninput = function() {
+              output1.innerHTML = this.value;
+          }
+          //messslider
+          var slider2 = document.getElementById("mess");
+          var output2 = document.getElementById("messoutput");
+          output2.innerHTML = slider2.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider2.oninput = function() {
+              output2.innerHTML = this.value;
+          }
+          //drinkslider
+          var slider3 = document.getElementById("drink");
+          var output3 = document.getElementById("drinkoutput");
+          output3.innerHTML = slider3.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider3.oninput = function() {
+              output3.innerHTML = this.value;
+          }
+          //tvslider
+          var slider4 = document.getElementById("tv");
+          var output4 = document.getElementById("tvoutput");
+          output4.innerHTML = slider4.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider4.oninput = function() {
+              output4.innerHTML = this.value;
+          }
+          //gamerslider
+          var slider5 = document.getElementById("gamer");
+          var output5 = document.getElementById("gameroutput");
+          output5.innerHTML = slider5.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider5.oninput = function() {
+              output5.innerHTML = this.value;
+          }
+          //gamerslider
+          var slider6 = document.getElementById("music");
+          var output6 = document.getElementById("musicoutput");
+          output6.innerHTML = slider6.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider6.oninput = function() {
+              output6.innerHTML = this.value;
+          }
+          //partyslider
+          var slider7 = document.getElementById("party");
+          var output7 = document.getElementById("partyoutput");
+          output7.innerHTML = slider7.value; // Display the default slider value
+          // Update the current slider value (each time you drag the slider handle)
+          slider7.oninput = function() {
+              output7.innerHTML = this.value;
+          }
+          //to make different parts of questionnaire hide/show
           var counter=0;
           $("#secondI").hide();
           $("#secondG").hide();
@@ -24,12 +83,10 @@
                   $("#button").hide();
               }
               if (counter==5){
-                  alert("you got offcamp");
                   $("#campus").hide();
                   $("#button").show();
                   $("#life").show();
               } else if (counter==4){
-                  alert("you got oncamp");
                   $("#campus").hide();
                   $("#button").show();
                   $("#life").show();
--- questionaire.php
+++ questionaire.php
@@ -16,10 +16,16 @@
         margin-top: 0px;
         background-color:darkred;
         font-family: 'Knewave', cursive;
-        }
+      }
+      .slidercontainer{
+          width: 25%;
+      }
       #first{
-          text-align: center;
-      }    
+        text-align: center;
+      } 
+      .center{
+        text-align: center;
+      }
       body{
           color: black;
           font-family: 'Roboto', sans-serif;
@@ -33,35 +39,33 @@
           color: white;
           text-align: center;
           font-size: 600%;
-          
       }
      h2{
           color: darkred;
           font-size: 400%;
           text-align: center;
           font-family: 'Knewave', cursive;
+          margin: 2%;
       }
-      
       .container{
+          margin-bottom:5%;
           padding: 3%;
-          background:rgba(255,255,255,0.6);
+          background:rgba(255,255,255,0.8);
           border-width: 10px 10px 10px 10px;
-          height: 550px;
           margin-top: 50px;
           border-style: solid;
-          border-color:rgba(139,0,0,0.6);
-          
+          border-color:rgba(139,0,0,0.8);
       }
-      li{
-          padding: 5%;
-          background:rgba(255,255,255,0.6);
+      /* Floats need to be cleared so the container will wrap correctly. */
+      div.clear {
+          clear:both;
       }
     </style>
   </head>
   <body >
       <h1 class="page-header">Form Q</h1>
         <div class="container">
-            <form action="testingidea.php" method="post">
+            <form action="questionaire.php" method="post">
                 <div id="first">
                     <h2>Grouping</h2> 
                     <p>Click on an image below to choose group or single</p>
@@ -70,32 +74,148 @@
                 </div>
                 <div id="campus">
                     <h2>On campus</h2>
-                    <p>Click on an image below to choose on campus or off campus living</p>
-                    <div class="col-sm-6"><img src="Barton.jpg" id="ocampus" style="width:300px;height:300px;"></div>
-                    <div class="col-sm-6"><img src="troy.jpg" id="offcampus" style="width:300px;height:300px;"></div>
+                    <p class="center">Click on an image below to choose on campus or off campus living</p>
+                    <div class="col-sm-6"><div class="center"><img src="Barton.jpg" id="ocampus" class="center" style="width:300px;height:300px;"></div></div>
+                    <div class="col-sm-6"><div class="center"><img src="troy.jpg" id="offcampus" style="width:300px;height:300px;"></div></div>
                 </div> 
                 <div id="secondI">
-                    <h2>Individual Information</h2>
-                    Name: <input type="text" name="name"><br>
-                    E-mail: <input type="text" name="email"><br>
+                    <h2>Individual Information</h2>  
+                    <div class="col-sm-6">          
+                        <p>Name:</p><input type="text" name="name">
+                        <p>E-mail:</p><input type="text" name="email">
+                        <p>Age:</p><input type="text" name="age">
+                        <p>Year in College:</p><select name="year">
+                          <option value="fresman">Freshman</option>
+                          <option value="sophmore">Sophmore</option>
+                          <option value="junior">Junior</option>
+                          <option value="senior">Senior</option>
+                          <option value="graduate">Graduate Student</option>
+                    </select>
+                    </div>
+                    <div class="col-sm-6"style="border-left: 5px solid darkred;">
+                        <p>What is your budget for housing per month?</p><input type="text" name="budget">
+                        <p>How many people are you looking for?</p><input type="text" name="number">
+                        <p>Gender: </p><input type="text" name="gender">
+                        <p>Co-Ed Housing?</p><select name="coed">
+                          <option value="coedyes">Yes</option>
+                          <option value="coedno">No</option>
+                        </select>
+                    </div>
                 </div>
                 <div id="secondG">
                     <h2>Group Information</h2>
-                    Name: <input type="text" name="name"><br>
-                    E-mail: <input type="text" name="email"><br>
+                    <div class="col-sm-6">
+                        <p>Name: </p><input type="text" name="gname">
+                        <p>E-mail: </p><input type="text" name="gemail">
+                        <p>Age: </p><input type="text" name="gage">
+                        <p>Year in College: </p><select name="gyear">
+                          <option value="gfresman">Freshman</option>
+                          <option value="gsophmore">Sophmore</option>
+                          <option value="gjunior">Junior</option>
+                          <option value="gsenior">Senior</option>
+                          <option value="ggraduate">Graduate Student</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-6"style="border-left: 5px solid darkred;">
+                        <p>What is your budget for housing per month? </p><input type="text" name="gbudget">
+                        <p>How many people are you looking for? </p><input type="text" name="gnumber">
+                        <p>Gender: </p><input type="text" name="ggender">
+                        <p>Co-Ed Housing? </p><select name="gcoed">
+                          <option value="gcoedyes">Yes</option>
+                          <option value="gcoedno">No</option>
+                        </select>
+                    </div>
                 </div>
                 <div id="life">
                     <h2>Life Style</h2>
-                    Name: <input type="text" name="name"><br>
-                    E-mail: <input type="text" name="email"><br>
+                    <div class = "col-sm-4">
+                        <p class="center">Please answer the following</p>
+                    </div>
+                    <div class = "col-sm-8">
+                        <p class="center">On a scale from 1-5 rank:</p>
+                    </div>
+                    <div class = "col-sm-4">
+                        <p>Any allergies? </p><input type="text" name="allergies">
+                        <p>Do you smoke? </p><select name="smoke">
+                          <option value="smokeyes">Yes</option>
+                          <option value="smokeno">No</option>
+                        </select>
+                        <p>What is the latest time you go to bed? </p> <select name="bedtime">
+                          <option value="bedtime8">8 PM</option>
+                          <option value="bedtime9">9 PM</option>
+                          <option value="bedtime10">10 PM</option>
+                          <option value="bedtime11">11 PM</option>
+                          <option value="bedtime12">12 AM</option>
+                          <option value="bedtime1">1 AM</option>
+                          <option value="bedtime2">2 AM</option>
+                          <option value="bedtime3">3 AM</option>
+                          <option value="bedtime4">4 AM</option>
+                          <option value="bedtime5">5 AM</option>
+                          <option value="bedtime6">6 AM</option>
+                        </select>
+                        <p>Morning Person or Night Person </p><select name="mornnight">
+                          <option value="morning">Morning</option>
+                          <option value="night">Night</option>
+                          <option value="mornightnopref">No Preference</option>
+                        </select>
+                        <p>Pets? </p><select name="mornnight">
+                          <option value="petsyes">Yes</option>
+                          <option value="petsno">No</option>
+                        </select>
+                    </div>
+                    <div class = "col-sm-4" style="border-left: 5px solid darkred;border-right: 5px solid darkred;">
+                        <p>How strict are you with sticking to a schedule?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="schedule">
+                            <div id="scheduleoutput">Output: </div>
+                        </div>
+                        <p>How much can you handle a mess?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="mess">
+                            <div id="messoutput">Output: </div>
+                        </div>
+                        <p>How much do you drink?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="drink">
+                            <div id="drinkoutput">Output: </div>
+                        </div>
+                        <p></p>
+                    </div>
+                    <div class = "col-sm-4">
+                        <p>How much TV do you watch?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="tv">
+                            <div id="tvoutput">Output: </div>
+                        </div>
+                        <p>How much of a gamer are you?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="gamer">
+                            <div id="gameroutput">Output: </div>
+                        </div>
+                        <p>How sensitive are you to loud music?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="music">
+                            <div id="musicoutput">Output: </div>
+                        </div>
+                        <p>How much do you party?</p><div class="slidercontainer">
+                            <input type="range" min="1" max="5" value="1" class="slider" id="party">
+                            <div id="partyoutput">Output: </div>
+                        </div>
+                        <p></p>
+                    </div>
                 </div>
                 <div id="pref">
                     <h2>Preferences</h2>
-                    Name: <input type="text" name="name"><br>
-                    E-mail: <input type="text" name="email"><br>
+                    <p>Do you have friends over often? </p><select name="friends">
+                      <option value="friendsall">All the time</option>
+                      <option value="friendsoften">Often</option>
+                      <option value="friendssome">Sometimes</option>
+                      <option value="friendsnever">Never</option>
+                    </select>
+                    <p>Do you want to host parties? </p><select name="friends">
+                      <option value="partiesall">All the time</option>
+                      <option value="partiessoften">Often</option>
+                      <option value="partiesssome">Sometimes</option>
+                      <option value="partiesnever">Never</option>
+                    </select>
                 </div>
             </form>
-            <button id="button">Submit</button>
+            <div class="center"><button id="button">Submit</button></div>
+            <div class="clear"></div>
       </div>
 
   </body>
