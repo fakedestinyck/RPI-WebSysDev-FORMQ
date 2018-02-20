30a1c47c7906ccc6504ade226657409aac11f3a1
Haochang Qian <fakedestinyck@gmail.com>
2018-02-20 13:44:56 -0500

    add auth

---
+++ index.php
@@ -0,0 +1,18 @@
+<?PHP
+
+    include_once("./CAS-1.3.5/CAS.php");
+    phpCAS::client(CAS_VERSION_2_0,'cas-auth.rpi.edu',443,'/cas/');
+    phpCAS::setDebug("log.log");
+    // SSL!
+    phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
+
+    if (phpCAS::isAuthenticated())
+    {
+        echo "Welcome to FORM Q, " . phpCAS::getUser(). "!";
+
+        echo "<a href='./logout.php'>Logout</a>";
+    }else{
+        echo "<a href='./login.php'>Login</a>";
+    }
+
+?>
