6a94829ceb6cf60c49ed7e780a5f0c342bcbddf8
ominhas <minhao@rpi.edu>
2018-03-05 01:27:06 -0500

    Made Home Page
    Made Home Page. Included edits to index.php and adding an index.css page.

--- index.php
+++ index.php
@@ -12,8 +12,32 @@
         //
         // echo "<a href='./logout.php'>Logout</a>";
         header('location: ./questionaire.php');
-    }else{
-        echo "<a href='./login.php'>Login</a>";
     }
 
 ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Form Q</title>
+        <script src="https://code.jquery.com/jquery-1.12.0.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Knewave" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+        <link href="index.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="full-wrapper">
+            <h1 class="page-header">Form Q</h1>
+            <div class ="center-wrapper">
+                <div class = "description">Welcome to Form Q. Answer a few questions, and we'll match you with a roommate instantly.</div><br>
+                <div class="btn-wrapper">
+                    <a href="login.php"><button type="button" class="btn btn-outline-secondary btn-lg log">Begin Your Search</button></a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
