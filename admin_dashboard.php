e0af2410427a30cca77493b5b8fffcac5af236be
ominhas <minhao@rpi.edu>
2018-03-26 13:04:22 -0400

    fixes

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -16,6 +16,8 @@
 </head>
 
 <body id="admin-body">
+
+	<div class ="page-wrap">
 	<nav id = "admin_nav" class = "navbar navbar-inverse navbar-fixed-top" data-spy="affix">
 		<div class = "container-fluid">
 			<div class = "navbar-header">
@@ -30,114 +32,117 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class = "nav navbar-nav navbar-right">
 					<li><a><span class = "glyphicon glyphicon-user"></span>Welcome, test_admin!</a></li>
+					<li><a href = "user_dashboard.php"><span>User Dashboard</span></a></li>
 					<li><a href = "logout.php"><span class = "glyphicon glyphicon-log-out"></span>Logout</a></li>
 				</ul>
 			</div>
 		</div>
 	</nav>
 
+	<h1 class ="hidden-xs" style="margin-left: 7%;">Admin Dashboard</h1>
+	<h3 class="hidden-lg hidden-md hidden-sm" style="margin-left: 7%;">Admin Dashboard</h3>
 	<div id="reported_users" class="panel panel-default">
-		<div class="panel-heading">Reported Users</div>
-		<ul class="list-group">
+		<div class="panel-heading" onclick="removeHidden()"">
+			Reported Users
+			<span id ="symbol" class="pull-right glyphicon glyphicon-plus hidden-lg hidden-md hidden-sm" style="top: 5px;"></span>
+		</div>
+		<ul id ="reported_content" class="list-group hidden-xs">
   			<li id ="0" class="list-group-item clearfix" style="word-wrap: break-word;">
-      			User 1
+      			<span>User 1</span>
       			<span class="pull-right">
       				<button class="btn btn-warning blackText" onclick="ignoreClick('0')">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash" onclick="blacklistClick('0')"></span>
-      				<button class="hidden-xs btn btn-danger blackText" onclick="blacklistClick('0')">Blacklist</button>
+      				<button class="btn btn-danger blackText" onclick="banClick('0')">Ban</button>
       			</span>
+      			<div class="text-muted" style="font-size: 15px;">Number of reports: X</div>
   			</li>
   			<li id="1" class="list-group-item">
   				User 2
   				<span class="pull-right">
       				<button class="btn btn-warning blackText" onclick="ignoreClick('1')">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash" onclick="blacklistClick('1')"></span>
-      				<button class="hidden-xs btn btn-danger blackText" onclick="blacklistClick('1')">Blacklist</button>
+      				<button class="btn btn-danger blackText" onclick="banClick('1')">Ban</button>
       			</span>
   			</li>
   			<li id="2" class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText" onclick="ignoreClick('2')">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash" onclick="blacklistClick('2')"></span>
-      				<button class="hidden-xs btn btn-danger blackText" onclick="blacklistClick('2')">Blacklist</button>
+      				<button class="btn btn-danger blackText" onclick="banClick('2')">Ban</button>
+      			</span>
+  			</li>
+   			<li class="list-group-item">
+  				User 3
+  				<span class="pull-right">
+      				<button class="btn btn-warning blackText" onclick="ignoreClick('3')">Ignore</button>
+      				<button class="btn btn-danger blackText" onclick="banClick('3')">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
    			<li class="list-group-item">
   				User 3
   				<span class="pull-right">
       				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
-      			</span>
-  			</li>
-   			<li class="list-group-item">
-  				User 3
-  				<span class="pull-right">
-      				<button class="btn btn-warning blackText">Ignore</button>
-      				<span class="hidden-lg hidden-md hidden-sm glyphicon glyphicon-trash"></span>
-      				<button class="hidden-xs btn btn-danger blackText">Blacklist</button>
+      				<button class="btn btn-danger blackText">Ban</button>
       			</span>
   			</li>
 
 		</ul>
 	</div>
-	<script type="text/javascript" src = "user_dashboard.js"></script>
+	</div>
+	<footer class="footer">
+		<div class="container">
+			<form action="aboutus.php">
+				<button class="btn btn-info" style="margin-top: 8px;">About Us</button>
+			</form>
+		</div>
+	</footer>
+	<script type="text/javascript" src = "admin_dashboard.js"></script>
 </body>
 </html>
--- user_dashboard.js
+++ user_dashboard.js
@@ -3,17 +3,5 @@ $(document).ready(function(){
 		$(this).parent().parent().slideUp("slow", function(){
 			alert("put code to remove from the database here");
 		});
-	}); 
+	});
 });
-
-function blacklistClick(idno) {
-	if (confirm("Are you sure you want to blacklist this user?")) {
-		$('#' + idno).hide();
-	}
-}
-
-function ignoreClick(idno) {
-	if (confirm("Are you sure you want to ignore this report?")) {
-		$('#' + idno).hide();
-	}
-}
