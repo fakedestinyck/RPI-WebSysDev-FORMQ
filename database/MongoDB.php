d4d5fe28729a60be34d12d964dab2afca814eb73
omerosman <omermosman97@gmail.com>
2018-04-12 13:34:53 -0400

    updated mongodb.php

--- database/MongoDB.php
+++ database/MongoDB.php
@@ -13,8 +13,11 @@
 
 	//create document
 	$document = array(
-		"user" => (object)array("user_id" => 1, "name" => "Omer Osman", "rin" => 661392198, "rcsid" => "osmano", "admin" => "yes"),
-		"group" => (object)array("group_id" => 1, "name" => "From Q", "current_num" => 2, "desired_num" => 4),
+		"user" => (object)array("user_id" => 1, "name" => "Omer Osman", "rin" => 661392198, "rcsid" => "osmano", "admin" => "yes", "in_group" => "yes"),
+		"group" => (object)array("group_id" => 1, "name" => "From Q", "current_num" => 2, "desired_num" => 4, 
+			"group_members" => (object)array("member1" => "", "member2" => "", "member3" => "", "member4" => "", "member5" => "", "member6" => "", "member7" => "", "member8" => "", "member9" => ""),
+			"group_answers" => (object)array("q1" => "Dust", "q2" => "smokeno", "q3" => "bedtime12", "q4" => "night", "q5" => "petsno", "q6" => 1, "q7" => 5, "q8" => 1, "q9" => 3, "q10" => 3, "q11" => 3, "q12" => 2, "q13" => "friendssome", "q14" => "partiesnever")
+		),
 		"profile" => (object)array("age" => 21, "year" => 2018, "budget" => 500, "gender" => "Male", "coed" => "no", "on/off campus" => "off campus"),
 		"answers" => (object)array("q1" => "Dust", "q2" => "smokeno", "q3" => "bedtime12", "q4" => "night", "q5" => "petsno", "q6" => 1, "q7" => 5, "q8" => 1, "q9" => 3, "q10" => 3, "q11" => 3, "q12" => 2, "q13" => "friendssome", "q14" => "partiesnever")
 	);
