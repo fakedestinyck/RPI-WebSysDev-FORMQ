1ee820c0c9aeec226599bb7c0fa085cfaca8d0f0
obrisa <obrisa@rpi.edu>
2018-04-21 12:16:12 -0400

    JS added to the filter functionality

--- search.js
+++ search.js
@@ -21,6 +21,42 @@
             $("#contentx").addClass("hidden");
             $("#expand").removeClass("hidden");
         }
-
+    });
+    preferences = [false,false,false,false,false,false,false,false,false,false,false];
+    $("#button").click(function(){
+        if($('#smoking').is(":checked")){
+            preferences[0]=true;
+        }
+        if($('#bedtime').is(":checked")){
+            preferences[1]=true;
+        }
+        if($('#mornnight').is(":checked")){
+            preferences[2]=true;
+        }
+        if($('#pets').is(":checked")){
+            preferences[3]=true;
+        }
+        if($('#schedule').is(":checked")){
+            preferences[4]=true;
+        }
+        if($('#mess').is(":checked")){
+            preferences[5]=true;
+        }
+        if($('#drink').is(":checked")){
+            preferences[6]=true;
+        }
+        if($('#party').is(":checked")){
+            preferences[7]=true;
+        }
+        if($('#tv').is(":checked")){
+            preferences[8]=true;
+        }
+        if($('#gamer').is(":checked")){
+            preferences[9]=true;
+        }
+        if($('#music').is(":checked")){
+            preferences[10]=true;
+        }
+        console.log(preferences);
     });
 });
