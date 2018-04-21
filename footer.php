99aeda9b12e487f6cfa121362b836b5b7aacd87f
ominhas <minhao@rpi.edu>
2018-04-21 12:27:06 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <div id="justdoit" class="container">
+    <div id="footcontainer" class="container">
         <div id="copyright">© 2018 Copyright: <a href ="aboutus.php">FORM Q</a> <span style="float: right;"><a href ="disclaimer.php">Disclaimer</a></span></div>
     </div>
 </footer>
