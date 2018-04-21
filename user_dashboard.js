4d0e7291c844d947279df8f56a84136eb0991188
obrisa <obrisa@rpi.edu>
2018-04-21 14:12:29 -0400

    Update

--- user_dashboard.js
+++ user_dashboard.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
 			alert("put code to remove from the database here");
 		});
 	});
+      membercounter = 1;
+      //ALLOWS USERS TO ENTER MORE RCS ids FOR ADDITIONAL TEAM MEMBERS
+      $("#addbutton").click(function(){
+          if (membercounter<=9){
+              membercounter +=1;
+              $("#toadd").append('<div class="form-group"><label for="groupmember' + membercounter + '">Enter another RCS ID:</label><div id="groupmembers"><input type="text" class="form-control" id="groupmember' + membercounter + '" placeholder="RCSID" name="groupmember' + membercounter + '" required></div></div>');
+          } else {
+              alert("You can't make a group larger than 10! Sorry")
+          }
+      });
+
 });
