f1357e95c713225cf4846a1d347981ff064528ee
obrisa <obrisa@rpi.edu>
2018-04-23 05:22:17 -0400

    Splitting search and match(please check division)
    Also added button redirect to match.php page

--- search.php
+++ search.php
@@ -30,93 +30,41 @@ $count = sizeof($allResults);
         <script type="text/javascript" src = "search.js"></script>
         <script type="text/javascript" src = "algo.js"></script>
     </head>
-    <body id = "bodyforNav">
+        <body id = "bodyforNav">
         <div class="page-wrap">
             <?php include_once('navbar.php'); ?>
             <div class = "container" id = "requests_container">
                 <h2 id = "req_heading">Matches</h2>
                 <div id = "requests" class = "panel-group">
-                <div class = "panel panel-danger" id = "request1">
-                    <div class = "panel-heading">
-                        <p>Please check qualities that are not important to you when searching for a roommate.  Multiple can be checked.</p>
-                        <input type="checkbox" class="form-check-input" id="smoking">
-                        <label class="form-check-label" for="smoking">Smoking</label>
-                        <input type="checkbox" class="form-check-input" id="bedtime">
-                        <label class="form-check-label" for="bedtime">Bedtime</label>
-                        <input type="checkbox" class="form-check-input" id="mornnight">
-                        <label class="form-check-label" for="mornnight">Morning or Night Person/People</label>
-                        <input type="checkbox" class="form-check-input" id="pets">
-                        <label class="form-check-label" for="pets">Pets</label>
-                        <input type="checkbox" class="form-check-input" id="schedule">
-                        <label class="form-check-label" for="schedule">Schedule</label>
-                        <input type="checkbox" class="form-check-input" id="mess">
-                        <label class="form-check-label" for="mess">Mess</label>
-                        <input type="checkbox" class="form-check-input" id="drink">
-                        <label class="form-check-label" for="drink">Drink Alcohol</label>
-                        <input type="checkbox" class="form-check-input" id="party">
-                        <label class="form-check-label" for="party">Partying</label>
-                        <input type="checkbox" class="form-check-input" id="tv">
-                        <label class="form-check-label" for="tv">TV</label>
-                        <input type="checkbox" class="form-check-input" id="gamer">
-                        <label class="form-check-label" for="gamer">Gaming</label>
-                        <input type="checkbox" class="form-check-input" id="music">
-                        <label class="form-check-label" for="music">Sensitivity to Loud Music</label>
-                        <div style="text-align: center;"><button id="button" style="background-color:white; color: black;" class="btn btn-primary">Submit</button></div>
+                    <div class = "panel panel-danger" id = "request1">
+                        <div class = "panel-heading" style="font-size: 200%;">
+                            <p>Please check qualities that are not important to you when searching for a roommate.  Multiple can be checked.</p>
+                            <input type="checkbox" class="form-check-input" id="smoking">
+                            <label class="form-check-label" for="smoking">Smoking</label>
+                            <input type="checkbox" class="form-check-input" id="bedtime">
+                            <label class="form-check-label" for="bedtime">Bedtime</label>
+                            <input type="checkbox" class="form-check-input" id="mornnight">
+                            <label class="form-check-label" for="mornnight">Morning or Night Person/People</label>
+                            <input type="checkbox" class="form-check-input" id="pets">
+                            <label class="form-check-label" for="pets">Pets</label>
+                            <input type="checkbox" class="form-check-input" id="schedule">
+                            <label class="form-check-label" for="schedule">Schedule</label>
+                            <input type="checkbox" class="form-check-input" id="mess">
+                            <label class="form-check-label" for="mess">Mess</label>
+                            <input type="checkbox" class="form-check-input" id="drink">
+                            <label class="form-check-label" for="drink">Drink Alcohol</label>
+                            <input type="checkbox" class="form-check-input" id="party">
+                            <label class="form-check-label" for="party">Partying</label>
+                            <input type="checkbox" class="form-check-input" id="tv">
+                            <label class="form-check-label" for="tv">TV</label>
+                            <input type="checkbox" class="form-check-input" id="gamer">
+                            <label class="form-check-label" for="gamer">Gaming</label>
+                            <input type="checkbox" class="form-check-input" id="music">
+                            <label class="form-check-label" for="music">Sensitivity to Loud Music</label>
+                            <div style="text-align: center;"><button id="button" style="background-color:white; color: black;" class="btn btn-primary"  onclick="window.location='match.php';">Submit</button></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-                <?php
-                $num = 1;
-                foreach ($allResults as $result) {
-                    $group = $result['group'];
-                    $answers = $group['group_answers'];
-                    echo '<div id="requests" class="panel panel-primary panel-group">';
-                    echo '<div class = "panel panel-danger" id = "request1">';
-                    echo '<div class="panel-heading">';
-
-                    echo '<h3 class="panel-title">'.$group['name'].'</h3>';
-
-//                    echo '<h3 class="panel-title">'.$group['name'].'<span style="float: right;" class="hidden-xs">example@email.com</span></h3>';
-                    echo 'Group Members: user1, user2, user3 ';
-//
-//                    echo 'Group Members: user1, user2, user3 <span style="float: right;" class="hidden-xs">Price: $100000</span>';
-//                    echo '<span class ="hidden-lg hidden-md hidden-sm"><br>Price: $1000000</span><br>';
-                    echo '<span class="pull-right clickable panel-collapsed"><i class="glyphicon glyphicon-chevron-down"></i></span>';
-//                echo '<div id = "expand">click to expand</div>';
-
-                    echo '</div>';
-
-                    echo '<div class = "panel-body" style="display: none"><div class="row">';
-                    echo '<div class="col-sm-6">';
-                    echo '<p><b>Ages: </b><span>20, 21, 22, 23, 24, 25, 26, 27, 28</p>';
-                    echo '<p><b>Years in College: </b><span>1, 2, 3, 4</span></p>';
-                    echo '<p><b># Roommates Looking For: </b><span>'.$group['desired_num'].'</span></p>';
-                    echo '<p><b>Co-ed: </b><span>Nein</span></p>';
-                    echo '<p><b>Allergies: </b><span>'.$answers['q1'].'</span></p>';
-                    echo '<p><b>Smoking: </b><span>'.($answers['q2'] == 'smokeno' ? 'No' : 'Yes').'</span></p>';
-                    echo '<p><b>Bedtime: </b><span>'.substr($answers['q3'],7).'</span></p>';
-                    echo '<p><b>Morning/Night Person(s): </b><span>'.$answers['q4'].'</span></p>';
-                    echo '<p><b>Pets: </b><span>'.($answers['q5'] == 'petsno' ? 'No' : 'Yes').'</span></p>';
-                    echo '</div>';
-                    echo '<div class="col-sm-6">';
-                    echo '<p><b>On a scale from 1-5 (5 being the most strict)</b></p>';
-                    echo '<p>Sticks to Schedules: <span class="pull-right">'.$answers['q6'].'</span></p>';
-                    echo '<p>Messiness: <span class="pull-right">'.$answers['q7'].'</span></p>';
-                    echo '<p>Alcholic Drinking: <span class="pull-right">'.$answers['q8'].'</span></p>';
-                    echo '<p>Partying: <span class="pull-right">'.$answers['q9'].'</span></p>';
-                    echo '<p>TV Watching: <span class="pull-right">'.$answers['q10'].'</span></p>';
-                    echo '<p>Gaming: <span class="pull-right">'.$answers['q11'].'</span></p>';
-                    echo '<p>Sensitvity to Music: <span class="pull-right">'.$answers['q12'].'</span></p>';
-                    echo '</div><br></div>';
-                    echo '<div class="form-group">';
-                    echo '<h2 style="color:black; font-size: 150%; text-align: left; margin: 1%;"><b>Notes</b></h2>notes Notes NOtes NOTes NOTEs NOTES';
-                    echo '</div>';
-                    echo '</div>';
-                    echo '</div>';
-                    echo '</div>';
-                }
-                ?>
-                <div style="text-align: center; "><button id="button" style="background-color:white; margin: 1%; color: black;" class="btn btn-primary" id="requests">Request to Join Group</button></div>
             </div>
         </div>
         <?php include_once('footer.php') ?>
