985423feb6abc3622e1d145a7495060369c49f87
Haochang Qian <fakedestinyck@gmail.com>
2018-04-26 11:45:11 -0400

    email request

--- api/leaveorspam.php
+++ api/leaveorspam.php
@@ -13,20 +13,16 @@ $d = $_GET['rcsid'];
 $dd = $dbo->selectSIS('users','user',array('rcsid'=>$self));
 $email = $dd[0]['user']['email'];
 $token = $_GET['token'];
-var_dump($d);
-
-var_dump($self);
-var_dump($email);
 if ($token == crypt($email.$self,$d)) {
     $s = $dbo->selectSIS('users','user',array('rcsid'=>$d));
     $du = $dd[0]['_id'];
     $dbo->updateSIS('users',array("current_num"=>$s[0]['group']['current_num']-1, "desired_num"=>$s[0]['group']['desired_num']+1),'group',array('group_id'=> $s[0]['group']['group_id']));
-    $dbo->updateSIS('users',array("group_id"=>0,"name"=>""),'group',array('rcsid'=>$self));
-    echo 'Successfully removed from group!';
+    $dbo->updateSIS('users',array("group_id"=>0,"name"=>""),'group',array(),array('_id'=>$du));
+    echo 'Successfully removed from group! <br>';
     if ($action == 'spam') {
         $success = $dbo->updateSIS('users',array('reported'=>'yes'),'user',array('rcsid'=>$d));
         if ($success) {
-            echo 'Successfully report user '.$d;
+            echo 'Successfully report user'.$d;
         }
     }
 } else {
